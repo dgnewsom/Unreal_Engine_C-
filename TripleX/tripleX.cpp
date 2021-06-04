1c63fcc7cf590a9d33b1ddf555a01225ca7c3ef2
Daniel Newsom <danielgnewsom@gmail.com>
2021-06-04 16:39:12 +0100

    Danny - TripleX complete

--- TripleX/tripleX.cpp
+++ TripleX/tripleX.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <ctime>
 
-void PlayGame()
+void PrintIntroduction(int DifficultyLevel)
 {
     //Introduction text
-    std::cout << "\nYou are a secret agent breaking into a secure server...\n";
+    std::cout << "\nYou are a secret agent breaking into a level " << DifficultyLevel <<  " secure server...\n";
     std::cout << "Enter the correct code to continue...\n";
+}
+
+bool PlayGame(int DifficultyLevel)
+{
+    PrintIntroduction(DifficultyLevel);
 
     //Declare code numbers
-    const int CodeA = 2, CodeB = 3, CodeC = 4;
+    const int CodeA = rand() % DifficultyLevel + DifficultyLevel, CodeB = rand() % DifficultyLevel + DifficultyLevel, CodeC = rand() % DifficultyLevel + DifficultyLevel;
 
     //Calculate sum and product
     int CodeSum = CodeA + CodeB + CodeC;
@@ -33,16 +39,34 @@ void PlayGame()
     if(GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
         std::cout << "\nCongratulations you're in!\n";
+        return true;
     }
     else
     {
-        std::cout << "\nUnlucky - You were caught!\n";
+        std::cout << "\nUnlucky - Try Again!\n";
+        return false;
     }
 }
 
 int main()
 {
-    PlayGame();
+    srand(time(NULL));
+
+    int Difficulty = 1;
+    int MaxDifficulty = 9;
+
+    while(Difficulty <= MaxDifficulty)
+    {
+        bool bLevelComplete = PlayGame(Difficulty);
+        std::cin.clear();
+        std::cin.ignore();
+        if(bLevelComplete)
+        {
+            ++Difficulty;
+        }
+    }
+
+    std::cout << "Congratulations you have completed the game!";
 
     return 0;
 }
