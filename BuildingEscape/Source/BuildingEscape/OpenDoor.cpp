590503ad3a8f9a4f81803739e8a13e8d441df2cc
Daniel Newsom <danielgnewsom@gmail.com>
2021-06-08 15:29:46 +0100

    Danny - Add Door opening script

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,43 @@
+// Copyright - Daniel Newsom 2021
+
+
+#include "OpenDoor.h"
+
+#include <string>
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	InitialYaw = GetOwner()->GetActorRotation().Yaw;
+	CurrentYaw = InitialYaw;
+	TargetYaw = InitialYaw + 90.f;
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	UE_LOG(LogTemp, Warning, TEXT("Yaw = %f"), GetOwner()->GetActorRotation().Yaw)
+	
+	CurrentYaw = GetOwner()->GetActorRotation().Yaw;
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = FMath::FInterpTo(CurrentYaw, TargetYaw, DeltaTime,OpeningSpeed);
+	GetOwner()->SetActorRotation(DoorRotation);
+	
+}
+
